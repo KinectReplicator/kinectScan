323dc67fed87b2a2a879937697c80be33c9c6288
Elie Rosen <mylifeiscomputers@gmail.com>
2013-02-14 14:37:42 -0500

    Starting the app design

---
+++ kinectScan_app.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kinectScan
+{
+    public partial class kinectScan_app : Form
+    {
+        public kinectScan_app()
+        {
+            InitializeComponent();
+        }
+    }
+}
