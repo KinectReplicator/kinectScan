d2678d893f616b8438040df756391b69eaafc5de
Elie Rosen <mylifeiscomputers@gmail.com>
2013-02-14 23:47:26 -0500

    starting to learn XAML and WPF this will be fun

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿
+namespace kinectScan
+{
+
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using Microsoft.Kinect;
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            /*if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }*/
+        }
+    }
+}
