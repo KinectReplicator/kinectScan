7608696b89370b35c5d84b96c9058b6ea38cfd8f
Elie Rosen <mylifeiscomputers@gmail.com>
2013-02-22 14:26:40 -0500

    Handle kinect sensor connection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,18 @@ namespace kinectScan
     /// </summary>
     public partial class MainWindow : Window
     {
+
+      
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor sensor;
+
+        /// <summary>
+        /// Drawing image that we will display
+        /// </summary>
+        private DrawingImage imageSource;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,7 +32,49 @@ namespace kinectScan
 
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
+           
+            // Create an image source that we can use to show depth
+            this.imageSource = new DrawingImage();
 
+            // Display the drawing using our image control
+            KinectDepthView.Source = this.imageSource;
+
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                //Start the depth stream
+                this.sensor.DepthStream.Enable();
+
+                // Add an event handler to be called whenever there is new depth frame data available
+                this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
+  
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+            if (null == this.sensor)
+            {
+                this.statusBarText.Text = Properties.Resources.NoKinectReady;
+            }
         }
 
         /// <summary>
@@ -30,10 +84,22 @@ namespace kinectScan
         /// <param name="e">event arguments</param>
         private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            /*if (null != this.sensor)
+            // Stop the sensor!
+            if (null != this.sensor)
             {
                 this.sensor.Stop();
-            }*/
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's DepthFrameReady event
+        /// 
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+
         }
     }
 }
